51a0455643adca197eed260b45e035c0b670bafe
Gianpietro Amigoni <amigoniomcg@gmail.com>
2021-07-15 13:56:10 +0200

    e

--- EyeshotDemo/DetailsForm.vb
+++ EyeshotDemo/DetailsForm.vb
@@ -1,7 +1,7 @@
 Public Class DetailsForm
 
     Private Sub closeButton_Click(sender As System.Object, e As System.EventArgs) Handles closeButton.Click
-        ''6d
+        ''6e
         Close()
     End Sub
 
